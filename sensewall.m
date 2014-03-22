e29f7a94062509360bf833097bc7d0619632aede
EilidhHendry <ehendry91@gmail.com>
2014-03-22 18:22:23 +0000

    robot can now move to wall and turn away when it hits it. Thresholds may need tweaking.

--- sensewall.m
+++ sensewall.m
@@ -1,3 +1,4 @@
+% Main script for making the robot navigate around walls/objects
 
 while 1
     
@@ -10,6 +11,7 @@ while 1
     back_sum = sum(sensor_values(7:8));
   
     % move forward until the distance from the front sensors is 900
+    % NOTE threshold may need adjusted
     while front_sum < 900
         
         % keep recalculating and displaying the sensor values
@@ -33,6 +35,7 @@ while 1
         
     % while the front sensor has something in front of it keep
     % recalculating the sensor values and turning left
+    % NOTE threshold may need adjusted
     while front_sum > 100
         
         sensor_values = readsensor()
@@ -42,6 +45,7 @@ while 1
         right_sum = sum(sensor_values(5:6));
         back_sum = sum(sensor_values(7:8));
         
+        % NOTE threshold may need adjusted
         if left_sum > 900
                         
             wb_differential_wheels_set_speed(-2, 2)
