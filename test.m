e3fe8f9a0ad601a20eb8fb27c4b739fac036b37e
superkom <angusscott@me.com>
2014-03-26 10:54:43 +0000

    Pushing up code,odoemtry and function to go near wall don't work :(

--- test.m
+++ test.m
@@ -1,6 +1,6 @@
 % Main script for making the robot navigate around walls/objects
 TIME_STEP = 64;
-ERROR_DISTANCE = 200;
+ERROR_DISTANCE = 400;
 DEFAULT_SPEED = 3;
 x = 0;
 y = 0;
@@ -12,8 +12,8 @@ while wb_robot_step(TIME_STEP) ~= -1
 
     sensor_values = readsensor();
     front_sum = sum(sensor_values(3:4));
-    left_sum = sum(sensor_values(1:3));
-    right_sum = sum(sensor_values(4:6));
+    left_sum = sum(sensor_values(1:2));
+    right_sum = sum(sensor_values(5:6));
     back_sum = sum(sensor_values(7:8));
   
     
@@ -23,19 +23,27 @@ while wb_robot_step(TIME_STEP) ~= -1
         right_speed = 4 - X;
         wb_differential_wheels_set_speed(left_speed,right_speed);
         wb_robot_step(TIME_STEP);
-        [x, y, phi] = odometry( x, y, phi, left_speed, right_speed )
+        [x, y, phi] = odometry( x, y, phi, left_speed, right_speed );
 
-       if sensor3_value > 0 && sensor4_value > 0
-           x = phi
-           while phi < x + pi
-               [x, y, phi] = odometry( x, y, phi, left_speed, right_speed)
-               wb_differential_wheels_set_speed(2,-2);
-           end
-       end
+%        if sensor3_value > 0 && sensor4_value > 0
+%            x = phi
+%            while phi < x + pi
+%                [x, y, phi] = odometry( x, y, phi, left_speed, right_speed)
+%                wb_differential_wheels_set_speed(2,-2);
+%            end
+%        end
     end
     
     while front_sum > 1
-        
+        if left_sum > right_sum
+
+            wb_differential_wheels_set_speed(2, -2)
+
+        elseif right_sum > left_sum
+
+            wb_differential_wheels_set_speed(-2, 2)
+
+        end
     end
     
 
