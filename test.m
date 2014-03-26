d1f6a2d905813b330bb3c3494f9a600e336078d9
EilidhHendry <ehendry91@gmail.com>
2014-03-26 16:39:46 +0000

    adding new world and tweaks

--- test.m
+++ test.m
@@ -1,11 +1,11 @@
 % Main script for making the robot navigate around walls/objects
 TIME_STEP = 64;
-ERROR_DISTANCE = 400;
+ERROR_DISTANCE = 200;
 DEFAULT_SPEED = 3;
 x = 0;
 y = 0;
 phi = 0;
-K = 0.001;
+K = 0.0001;
 
 
 while wb_robot_step(TIME_STEP) ~= -1
@@ -17,7 +17,14 @@ while wb_robot_step(TIME_STEP) ~= -1
     back_sum = sum(sensor_values(7:8));
   
     
-    while front_sum < 1 
+    while front_sum < 1
+        
+        sensor_values = readsensor();
+        front_sum = sum(sensor_values(3:4));
+        left_sum = sum(sensor_values(1:2));
+        right_sum = sum(sensor_values(5:6));
+        back_sum = sum(sensor_values(7:8));
+    
         X = K*(left_sum - ERROR_DISTANCE)
         left_speed = 4; 
         right_speed = 4 - X;
@@ -34,17 +41,28 @@ while wb_robot_step(TIME_STEP) ~= -1
 %        end
     end
     
-    while front_sum > 1
-        if left_sum > right_sum
-
+    % stop
+    wb_differential_wheels_set_speed(0, 0)
+       
+    %while front_sum > 999
+               
+        sensor_values = readsensor();
+        front_sum = sum(sensor_values(3:4));
+        left_sum = sum(sensor_values(1:2));
+        right_sum = sum(sensor_values(5:6));
+        back_sum = sum(sensor_values(7:8));
+        
+        front_sum
+        if left_sum+sensor_values(3) > right_sum+sensor_values(4)
+            left_sum+sensor_values(3)
             wb_differential_wheels_set_speed(2, -2)
 
-        elseif right_sum > left_sum
-
+        elseif right_sum+sensor_values(4) > left_sum+sensor_values(3)
+            right_sum+sensor_values(4)
             wb_differential_wheels_set_speed(-2, 2)
 
         end
-    end
+    %end
     
 
 
